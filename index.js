8dad5a75d10b056d8cda88c351cdfa6b7eb12bfa
Sharon Wang <sharon-wang-cpsc@outlook.com>
2020-08-18 19:13:13 -0400

    Implement getContact and addContact HTTP requests
    Signed-off-by: Sharon Wang <sharon-wang-cpsc@outlook.com>

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 window.addEventListener('load', () => {
 	async function addContact() {
 		const data = {
-			"payload": "hello this is a test payload",
-			"random_number": 823472984,
-			"location": "Sharon's living room",
-			"email": "hello@email.mail"
+			'firstName': add_contact_first_name_input.value,
+			'lastName': add_contact_last_name_input.value,
+			'email': add_contact_email_input.value
 		};
-		// data will get replaced with simple first name, last name, email from the form
+
 		const response = await submitPOSTRequest(data);
-	}
 
-	async function getContact() {
-		// const url_search_params = new URLSearchParams({
-		// 	// data will get replaced with simple first name, last name from the form
-		// });
-		// const response = await submitHTTPRequest(serverEndpointGETurl + '?' + url_search_params);
-		const response = await submitGETRequest(serverEndpointGETurl + '?firstName=Igor&lastName=Braga');
-
-		if (response.status == '200') {
-			get_contact_data_p.innerText = response.json();
-			// get_contact_data_p.innerText = 'Sam Cloud\nsamcloud@cloudready.app';
+		if (response.ok) {
+			add_contact_data_p.innerText = response.json();
+		} else {
+			add_contact_data_p.innerText = 'Request failed with: ' + response.statusText;
 		}
 	}
 
+	async function getContact() {
+		const url_search_params = new URLSearchParams({
+			'firstName': get_contact_first_name_input.value,
+			'lastName': get_contact_last_name_input.value
+		});
+
+		const response = await submitGETRequest(serverEndpointGETurl + '?' + url_search_params);
+		// const response = await submitGETRequest(serverEndpointGETurl + '?firstName=Igor&lastName=Braga');
+
+		if (response.ok) {
+			get_contact_data_p.innerText = response.json();
+		} else {
+			get_contact_data_p.innerText = 'Request failed with: ' + response.statusText;
+		}
+	}
+
+	// TODO: error handling
 	async function submitGETRequest(url = '') {
 		const response = await fetch(url, {
 			method: 'GET',
@@ -35,6 +44,7 @@ window.addEventListener('load', () => {
 		return response;
 	}
 
+	// TODO: error handling
 	async function submitPOSTRequest(requestBody = {}) {
 		const response = await fetch(serverEndpointPOSTurl, {
 			method: 'POST',
@@ -48,21 +58,34 @@ window.addEventListener('load', () => {
 		return response;
 	}
 
+	// addContact elements
 	const add_contact_form = document.getElementById('add_contact_form');
+	const add_contact_first_name_input = document.getElementById('add_contact_first_name_input');
+	const add_contact_last_name_input = document.getElementById('add_contact_last_name_input');
+	const add_contact_email_input = document.getElementById('add_contact_email_input');
+	const add_contact_data_p = document.getElementById('add_contact_data_p');
+
+	// getContact elements
 	const get_contact_form = document.getElementById('get_contact_form');
+	const get_contact_first_name_input = document.getElementById('get_contact_first_name_input');
+	const get_contact_last_name_input = document.getElementById('get_contact_last_name_input');
 	const get_contact_data_p = document.getElementById('get_contact_data_p');
 
+	// Server Endpoints
 	const serverEndpointGETurl = 'http://169.51.194.167:30422/HelloCloudDemoProject/demo/database/retrieve';
 	const serverEndpointPOSTurl = 'https://169.51.194.167:30565/HelloCloudDemoProject/demo/database/store/document';
 
 	// https://169.51.194.167:30565/HelloCloudDemoProject/demo/database/retrieve/randomEmail@123.ca
 	// https://169.51.194.167:30565/HelloCloudDemoProject/demo/database/store/document
 
+
+	// Add contact form submit listener
 	add_contact_form.addEventListener('submit', (event) => {
 		event.preventDefault(); // Do not execute the default event handling, since we want our custom HTTP request handling
 		addContact();
 	});
 
+	// Get contact form submit listener
 	get_contact_form.addEventListener('submit', (event) => {
 		event.preventDefault(); // Do not execute the default event handling, since we want our custom HTTP request handling
 		getContact();
